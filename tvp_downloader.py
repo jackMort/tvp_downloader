d735bd0d6ce77126186563b48c4b070beaa2ea06
jackMort <lech.twarog@gmail.com>
2013-10-27 20:21:10 +0100

    PEP8

--- tvp_downloader.py
+++ tvp_downloader.py
@@ -1,9 +1,10 @@
 import re
-import sys
 import json
 import urllib2
 import argparse
 
+
+URL_TEMPLATE = 'http://www.tvp.pl/shared/cdn/tokenizer_v2.php?object_id=%s'
 
 parser = argparse.ArgumentParser(description='Download tvp.pl video')
 parser.add_argument('url', action='store', metavar='URL')
@@ -13,10 +14,10 @@ args = parser.parse_args()
 try:
     response = urllib2.urlopen(args.url)
     html = response.read()
-    match = re.search (r"object_id:'(.*)'", html)
+    match = re.search(r"object_id:'(.*)'", html)
     if match:
         id = match.group(1)
-        response = urllib2.urlopen('http://www.tvp.pl/shared/cdn/tokenizer_v2.php?object_id=%s' % id)
+        response = urllib2.urlopen(URL_TEMPLATE % id)
         result = json.loads(response.read())
 
         url = result.get('url')
@@ -29,7 +30,7 @@ try:
         f = open(file_name, 'wb')
         meta = u.info()
         file_size = int(meta.getheaders("Content-Length")[0])
-        
+
         file_size_dl = 0
         block_sz = 8192
         while True:
@@ -40,12 +41,11 @@ try:
             file_size_dl += len(buffer)
             f.write(buffer)
             p = float(file_size_dl) / file_size
-            status = "Downloading: {0} Bytes: {1}, {2}  [{3:.2%}]".format(url, file_size, file_size_dl, p)
-            status = status + chr(8)*(len(status)+1)
-            #sys.stdout.write(status)
-            print status
-
+            status = "Downloading: {0} Bytes: {1}, {2}  [{3:.2%}]".format(
+                url, file_size, file_size_dl, p)
+            print status + chr(8)*(len(status)+1)
         f.close()
+
 except urllib2.HTTPError, e:
     if e.code == 404:
         print "invalid url: ", e.url
